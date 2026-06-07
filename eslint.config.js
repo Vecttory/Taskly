137cab001722b4ac16f79f62affbe3f893de734d
Vecttory <alberto.ola.dev@gmail.com>
2026-06-07 12:48:09 -0600

    react native plugin added and componen refactored

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,23 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const reactNativePlugin = require("eslint-plugin-react-native");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
+  },
+  {
+    plugins: {
+      "react-native": reactNativePlugin,
+    },
+    rules: {
+      "react-native/no-unused-styles": "error",
+      "react-native/no-inline-styles": "warn",
+      "react-native/no-color-literals": "warn",
+      "react-native/no-raw-text": "error",
+    },
   },
 ]);
