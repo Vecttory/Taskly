eacdd28e87ad58bf7abef33855befbd0097d237b
Alberto Ola <alberto.ola.dev@gmail.com>
2026-06-08 14:09:42 -0600

    testing of unsigned commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { ShoppingListItem } from "./components/ShoppingListItem";
 export default function App() {
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Starbucks" isCompleted />
     </View>
