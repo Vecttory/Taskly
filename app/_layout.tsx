cb0d906cdc3d1dd1b1dc3397827352153fb5889a
Alberto Ola <alberto.ola.dev@gmail.com>
2026-06-09 14:58:09 -0600

    added nested navigation stack and refactor with new files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,43 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-      <Stack.Screen name="counter" options={{ title: "Counter" }} />
-      <Stack.Screen name="idea" options={{ title: "My Ideas" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="unordered-list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clock-circle" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "My Ideas",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="lightbulb-alert-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,10 @@
 import { theme } from "../theme";
 import { StyleSheet, View } from "react-native";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{ textAlign: "center", marginBottom: 20, fontSize: 18 }}
-      >
-        Go to Counter
-      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Starbucks" isCompleted />
