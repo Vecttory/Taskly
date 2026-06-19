f86dc7ae2291333eaa9a8497800b44a27fc594fb
Alberto Ola <alberto.ola.dev@gmail.com>
2026-06-18 18:45:50 -0600

    interval added and date library added

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,53 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
 
 export default function CounterScreen() {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((val) => val + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  const handleRequestPermissions = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app 📨",
+        },
+        trigger: {
+          type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+          seconds: 5, // Triggers 5 seconds from now
+          repeats: false,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notification",
+          "Enable the notification permission for Expo Go in settings",
+        );
+      }
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <Text>{secondsElapsed}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleRequestPermissions}
+      >
+        <Text style={styles.buttonText}>Send Notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,7 +59,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: theme.colorWhite,
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
